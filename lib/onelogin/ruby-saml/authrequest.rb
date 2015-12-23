b4d59f24cdcca34dc0a39b5ef40901823c0b2973
Josh King <actaneon@gmail.com>
2015-12-23 11:36:33 -0800

    Switch from CGI.escape to URI.escape because CGI seems to blow up on some long strings with certain chars (/ or +) and return nothing

--- lib/onelogin/ruby-saml/authrequest.rb
+++ lib/onelogin/ruby-saml/authrequest.rb
@@ -31,7 +31,7 @@ module OneLogin
       def create(settings, params = {})
         params = create_params(settings, params)
         params_prefix = (settings.idp_sso_target_url =~ /\?/) ? '&' : '?'
-        saml_request = CGI.escape(params.delete("SAMLRequest"))
+        saml_request = URI.escape(params.delete("SAMLRequest"))
         request_params = "#{params_prefix}SAMLRequest=#{saml_request}"
         params.each_pair do |key, value|
           request_params << "&#{key.to_s}=#{CGI.escape(value.to_s)}"
